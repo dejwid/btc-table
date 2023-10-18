25f75f72f6788003f99d9b6c9432addad81b9ad2
Dawid Paszko <dawid.paszko@gmail.com>
2023-10-18 18:30:16 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,7 @@ function App() {
 
   return (
     <main className="max-w-2xl mx-auto px-4 py-8">
-      {/*
-<h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">Find cheapest BTC</h1>
+      <h1 className="uppercase text-6xl text-center font-bold bg-gradient-to-br from-purple-600 to-sky-400 bg-clip-text text-transparent from-30%">Find cheapest BTC</h1>
       <div className="flex justify-center mt-6">
         <AmountInput
           value={amount}
@@ -77,8 +76,6 @@ function App() {
           />
         ))}
       </div>
-      */}
-    
     </main>
   )
 }
